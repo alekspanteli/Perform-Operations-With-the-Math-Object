9855f3f5d8315d9020ac7dc1fd1e585e186589ad
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-09-12 17:00:18 +0300

    1 file modified

--- script.js
+++ script.js
@@ -11,5 +11,8 @@
 // Round number number up
 // console.log(Math.ceil(49.9));
 
+// Random number between 0 and 4
+// console.log(Math.floor(Math.random() * 4));
 
-console.log(Math.floor(Math.random() * 4));
+// Random number between 1 and 4
+// console.log(Math.floor(Math.random() * 4)) + 1;
