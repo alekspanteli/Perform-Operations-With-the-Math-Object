6bb7609c993773aaadb9263389090b5f790ea749
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-09-12 16:53:23 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-wkygek
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-wkygek)
---
+++ script.js
@@ -0,0 +1,15 @@
+
+// Rounds up or rounds down number
+// console.log(Math.round(49.9));
+
+// Generate number between 0 and 6
+// console.log(Math.random(2) * 8);
+
+// Round number number down
+// console.log(Math.floor(49.9));
+
+// Round number number up
+// console.log(Math.ceil(49.9));
+
+
+console.log(Math.floor(Math.random() * 4));
